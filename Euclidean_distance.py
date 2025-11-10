01914ef1a6d4f45de12cb611ed5d7b2701ad0a1e
Raining-Li <95346997+Raining-Li@users.noreply.github.com>
2025-11-10 16:59:33 +0800

    Update Euclidean_distance.py

--- Euclidean_distance.py
+++ Euclidean_distance.py
@@ -2,22 +2,23 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(r"F:\allvar_normalized.txt", sep='\t')  # 请替换为实际文件路径
+df = pd.read_csv(r"F:\allvar_normalized.txt", sep='\t')  
 
 UQA = df['UQA'].values
 
 euclidean_distances = {}
 
 for column in df.columns:
-    if column != 'basin_var' and column != 'UQA':  # 排除非变量列和 'kalakashi_mean' 本身
-        # 获取其他列数据
+    if column != 'basin_var' and column != 'UQA': 
+
         other_column = df[column].values
-        # 计算欧几里得距离
+        # Euclidean distance
         distance = np.sqrt(np.sum((UQA - other_column) ** 2))
         # print((UQA - other_column) ** 2)
         euclidean_distances[column] = distance
 
-# 打印计算出的欧几里得距离
+
 for column, distance in euclidean_distances.items():
 
     print(f"Euclidean distance between 'UQA' and '{column}': {distance}")
+
